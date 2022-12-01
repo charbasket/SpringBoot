d8c893a7ca5c7d9619962ed379923a596d6cc5fd
charbasket <char_basket@hotmail.com>
2022-12-01 20:28:06 +0100

    Update Student.java

--- spring-mongodb/src/main/java/com/carlosfuentes/springmongodb/entity/Student.java
+++ spring-mongodb/src/main/java/com/carlosfuentes/springmongodb/entity/Student.java
@@ -1,4 +1,38 @@
 package com.carlosfuentes.springmongodb.entity;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Document
 public class Student {
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    @Indexed(unique = true)
+    private String email;
+    private Gender gender;
+    private Adress address;
+    private List<String> favouritesSubjects;
+    private BigDecimal totalSpentInBooks;
+    private LocalDateTime createdAt;
+
+    public Student(String firstName, String lastName, String email, Gender gender, Adress address,
+                   List<String> favouritesSubjects, BigDecimal totalSpentInBooks, LocalDateTime createdAt) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.gender = gender;
+        this.address = address;
+        this.favouritesSubjects = favouritesSubjects;
+        this.totalSpentInBooks = totalSpentInBooks;
+        this.createdAt = createdAt;
+    }
 }
