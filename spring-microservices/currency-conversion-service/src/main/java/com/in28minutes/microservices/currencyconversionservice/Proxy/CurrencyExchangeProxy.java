a7d7515beca7a3f3a04d2a4cc709c901648e9ec5
charbasket <char_basket@hotmail.com>
2023-01-20 13:51:24 +0100

    Connecting with Eureka Server

--- spring-microservices/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/Proxy/CurrencyExchangeProxy.java
+++ spring-microservices/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/Proxy/CurrencyExchangeProxy.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 // Create a Proxy so the calls to the CurrencyExchange Service are easier
 // We need to annotate @EnableFeignClients on the main class for this to work
 // It needs to have the same name as the CurrencyExchange service defined on properties
-@FeignClient(name = "currency-exchange", url = "localhost:8000")
+
+// Now we have Eureka server, so we delete the URL and the Eureka Server will do the load balancing for us
+// @FeignClient(name = "currency-exchange", url = "localhost:8000")
+@FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeProxy {
 
     // Because CurrencyExchange and CurrencyConversion structure matches it passes the values automatically
